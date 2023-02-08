bd32769ced1f1d7874ee6767134da716be35c677
mhubii <m.huber_1994@hotmail.de>
2023-02-08 09:04:45 +0000

    added launch

---
+++ launch/ros2_whisper.launch.py
@@ -0,0 +1,28 @@
+from launch_ros.actions import Node
+
+from launch import LaunchDescription
+from launch.actions import RegisterEventHandler
+from launch.event_handlers import OnProcessStart
+
+
+def generate_launch_description():
+
+    audio_command_node = Node(
+        package="ros2_whisper",
+        executable="audio_command_node",
+        output="screen",
+    )
+
+    whisper_inference_node = Node(
+        package="ros2_whisper",
+        executable="whisper_inference_node",
+        output="screen",
+    )
+
+    whisper_inference_node_event_handler = RegisterEventHandler(
+        OnProcessStart(
+            on_start=[whisper_inference_node], target_action=audio_command_node
+        )
+    )
+
+    return LaunchDescription([audio_command_node, whisper_inference_node_event_handler])
--- setup.py
+++ setup.py
@@ -9,6 +9,7 @@ setup(
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
         ("share/" + package_name, ["package.xml"]),
+        ("share/" + package_name, ["launch/ros2_whisper.launch.py"]),
     ],
     install_requires=["setuptools"],
     zip_safe=True,
