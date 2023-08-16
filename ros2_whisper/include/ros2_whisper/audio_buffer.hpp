e0a68c69715ff40f2af286c6b02e4065ca874de4
mhubii <m.huber_1994@hotmail.de>
2023-08-16 23:30:56 +0100

    added a normalization getter

--- ros2_whisper/include/ros2_whisper/audio_buffer.hpp
+++ ros2_whisper/include/ros2_whisper/audio_buffer.hpp
@@ -1,6 +1,7 @@
 #ifndef ROS2_WHISPER__AUDIO_BUFFER_HPP_
 #define ROS2_WHISPER__AUDIO_BUFFER_HPP_
 
+#include <algorithm>
 #include <cstdint>
 #include <vector>
 
@@ -11,6 +12,7 @@ public:
   void add_audio_data(const std::vector<std::int16_t> &audio_data);
   void clear_audio_data();
   inline const std::vector<std::int16_t> &get_audio_data() const { return audio_data_; };
+  std::vector<float> get_normalized_audio_data() const;
   inline std::size_t get_audio_data_size() const { return audio_data_.size(); };
 
 protected:
--- ros2_whisper/src/audio_buffer.cpp
+++ ros2_whisper/src/audio_buffer.cpp
@@ -7,5 +7,16 @@ void AudioBuffer::add_audio_data(const std::vector<std::int16_t> &audio_data) {
   audio_data_.insert(audio_data_.end(), audio_data.begin(), audio_data.end());
 }
 
+std::vector<float> AudioBuffer::get_normalized_audio_data() const {
+  std::vector<float> normalized_audio_data;
+  int i = 0;
+  std::for_each(audio_data_.begin(), audio_data_.end(), [&](const std::int16_t &audio_data) {
+    normalized_audio_data.push_back(static_cast<float>(audio_data) /
+                                    32768.); // normalize to [-1, 1]
+    ++i;
+  });
+  return normalized_audio_data;
+}
+
 void AudioBuffer::clear_audio_data() { audio_data_.clear(); }
-} // namespace ros2_whisper
+} // end of namespace ros2_whisper
