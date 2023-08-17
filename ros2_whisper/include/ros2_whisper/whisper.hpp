e72924aaedb00bd864b4e9e044028eb706c2ee9d
mhubii <m.huber_1994@hotmail.de>
2023-08-17 01:51:08 +0100

    added parameter interface

--- ros2_whisper/include/ros2_whisper/whisper.hpp
+++ ros2_whisper/include/ros2_whisper/whisper.hpp
@@ -5,19 +5,30 @@
 #include <string>
 #include <vector>
 
+#include "rclcpp/rclcpp.hpp"
+
 #include "whisper.h"
+
+#include "ros2_whisper/model_manager.hpp"
 
 namespace ros2_whisper {
 class Whisper {
 public:
-  Whisper();
+  Whisper(const rclcpp::node_interfaces::NodeParametersInterface::SharedPtr parameter_interface);
   ~Whisper();
 
   std::vector<std::string> forward(const std::vector<float> &input);
 
 protected:
+  void initialize_parameters_();
+  void initialize_model_();
+
+  rclcpp::node_interfaces::NodeParametersInterface::SharedPtr parameter_interface_;
+
   whisper_context *wctx_;
   whisper_full_params wparams_;
+
+  ModelManager model_manager_;
 };
 } // end of namespace ros2_whisper
 
