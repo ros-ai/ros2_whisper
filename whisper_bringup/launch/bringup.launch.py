3c00f542d599502c8c07bd333308eef1ccc1d4e3
mhubii <m.huber_1994@hotmail.de>
2023-08-17 13:40:03 +0100

    added simple bringup

--- whisper_bringup/launch/bringup.launch.py
+++ whisper_bringup/launch/bringup.launch.py
@@ -1,17 +1,30 @@
 from launch import LaunchDescription
-
-from whisper_launch_mixin import WhisperMixin, AudioBufferMixin
+from launch_ros.actions import Node
+from whisper_launch_mixin import AudioBufferMixin, WhisperMixin
 
 
 def generate_launch_description() -> LaunchDescription:
     ld = LaunchDescription()
+
+    # launch audio listener
+    ld.add_action(
+        Node(
+            package="audio_listener",
+            executable="audio_listener_node",
+            output="screen",
+        )
+    )
+
+    # launch whisper
     ld.add_action(WhisperMixin.arg_model_name())
     ld.add_action(WhisperMixin.arg_n_threads())
     ld.add_action(WhisperMixin.arg_language())
     ld.add_action(
         WhisperMixin.composable_node_container_whisper(
             composable_node_descriptions=[
-                AudioBufferMixin.composable_node_audio_buffer(),
+                AudioBufferMixin.composable_node_audio_buffer(
+                    remappings=[("/audio_buffer/audio", "/audio_listener_node/audio")],
+                ),
                 WhisperMixin.composable_node_whisper(
                     remappings=[
                         ("/whisper/provide", "/audio_buffer/provide"),
