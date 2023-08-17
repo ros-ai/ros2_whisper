eb86429ada38e9ab8e7e1d32ccef03c15ac62d7e
mhubii <m.huber_1994@hotmail.de>
2023-08-17 18:06:32 +0100

    added empty record action server

--- whisper_nodes/include/whisper_nodes/audio_buffer_node.hpp
+++ whisper_nodes/include/whisper_nodes/audio_buffer_node.hpp
@@ -2,31 +2,39 @@
 #define WHISPER_NODES__AUDIO_BUFFER_NODE_HPP_
 
 #include <atomic>
+#include <thread>
 
 #include "rclcpp/rclcpp.hpp"
+#include "rclcpp_action/rclcpp_action.hpp"
 #include "std_msgs/msg/int16_multi_array.hpp"
 #include "std_msgs/msg/multi_array_dimension.hpp"
-#include "std_srvs/srv/set_bool.hpp"
-#include "std_srvs/srv/trigger.hpp"
 
-#include "whisper_msgs/srv/provide_audio.hpp"
+#include "whisper_msgs/action/record_audio.hpp"
 #include "whisper_wrapper/audio_buffer.hpp"
 
 namespace whisper {
 class AudioBufferNode {
+  using RecordAudioAction = whisper_msgs::action::RecordAudio;
+  using RecordAudioGoalHandle = rclcpp_action::ServerGoalHandle<RecordAudioAction>;
+
 public:
   AudioBufferNode(const rclcpp::Node::SharedPtr node_ptr);
 
 protected:
+  rclcpp_action::GoalResponse
+  on_record_audio_goal_(const rclcpp_action::GoalUUID &uuid,
+                        const RecordAudioAction::Goal::ConstSharedPtr goal);
+  rclcpp_action::CancelResponse
+  on_record_audio_cancel_(const std::shared_ptr<RecordAudioGoalHandle> goal_handle);
+  void on_record_audio_accepted_(const std::shared_ptr<RecordAudioGoalHandle> goal_handle);
+
+  void record_audio_(const std::shared_ptr<RecordAudioGoalHandle> goal_handle);
+
   rclcpp::Node::SharedPtr node_ptr_;
 
   std::atomic_bool record_;
-
-  rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr record_service_;
-  rclcpp::Service<std_srvs::srv::Trigger>::SharedPtr clear_service_;
-  rclcpp::Service<whisper_msgs::srv::ProvideAudio>::SharedPtr provide_service_;
-
   rclcpp::Subscription<std_msgs::msg::Int16MultiArray>::SharedPtr audio_subscription_;
+  rclcpp_action::Server<RecordAudioAction>::SharedPtr record_audio_action_server_;
 
   AudioBuffer audio_buffer_;
 };
--- whisper_nodes/src/audio_buffer_node.cpp
+++ whisper_nodes/src/audio_buffer_node.cpp
@@ -1,47 +1,8 @@
-
 #include "whisper_nodes/audio_buffer_node.hpp"
 
 namespace whisper {
 AudioBufferNode::AudioBufferNode(const rclcpp::Node::SharedPtr node_ptr)
     : node_ptr_(node_ptr), record_(false) {
-
-  // rclcpp::Service<std_srvs::srv::SetBool>::SharedPtr record_service_;
-  record_service_ = node_ptr_->create_service<std_srvs::srv::SetBool>(
-      "~/record", [this](const std_srvs::srv::SetBool::Request::SharedPtr request,
-                         std_srvs::srv::SetBool::Response::SharedPtr response) {
-        record_ = request->data;
-        response->success = true;
-        response->message = "Recording set to " + std::to_string(record_) + ".";
-      });
-
-  clear_service_ = node_ptr_->create_service<std_srvs::srv::Trigger>(
-      "~/clear", [this](const std_srvs::srv::Trigger::Request::SharedPtr,
-                        std_srvs::srv::Trigger::Response::SharedPtr response) {
-        audio_buffer_.clear_audio_data();
-        response->success = true;
-        response->message = "Audio buffer cleared.";
-      });
-
-  // rclcpp::Service<whisper_msgs::srv::ProvideAudio> provide_service_;
-  provide_service_ = node_ptr_->create_service<whisper_msgs::srv::ProvideAudio>(
-      "~/provide", [this](const whisper_msgs::srv::ProvideAudio::Request::SharedPtr,
-                          whisper_msgs::srv::ProvideAudio::Response::SharedPtr response) {
-        if (audio_buffer_.get_audio_data_size() == 0) {
-          response->success = false;
-          response->info = "No audio data available.";
-          RCLCPP_WARN(node_ptr_->get_logger(), response->info.c_str());
-          return;
-        }
-        response->success = true;
-        response->info = "Audio data provided.";
-        std_msgs::msg::MultiArrayDimension dim;
-        dim.label = "audio";
-        dim.size = audio_buffer_.get_audio_data_size();
-        dim.stride = 1;
-        response->audio.layout.data_offset = 0;
-        response->audio.layout.dim.push_back(dim);
-        response->audio.data = audio_buffer_.get_normalized_audio_data();
-      });
 
   audio_subscription_ = node_ptr_->create_subscription<std_msgs::msg::Int16MultiArray>(
       "~/audio", 10, [this](const std_msgs::msg::Int16MultiArray::SharedPtr msg) {
@@ -49,5 +10,25 @@ AudioBufferNode::AudioBufferNode(const rclcpp::Node::SharedPtr node_ptr)
           audio_buffer_.add_audio_data(msg->data);
         }
       });
+
+  record_audio_action_server_ = rclcpp_action::create_server<RecordAudioAction>(
+      node_ptr_, "~/record",
+      std::bind(&AudioBufferNode::on_record_audio_goal_, this, std::placeholders::_1,
+                std::placeholders::_2),
+      std::bind(&AudioBufferNode::on_record_audio_cancel_, this, std::placeholders::_1),
+      std::bind(&AudioBufferNode::on_record_audio_accepted_, this, std::placeholders::_1));
 }
+
+rclcpp_action::GoalResponse
+AudioBufferNode::on_record_audio_goal_(const rclcpp_action::GoalUUID &uuid,
+                                       const RecordAudioAction::Goal::ConstSharedPtr goal) {}
+
+rclcpp_action::CancelResponse
+AudioBufferNode::on_record_audio_cancel_(const std::shared_ptr<RecordAudioGoalHandle> goal_handle) {
+}
+
+void AudioBufferNode::on_record_audio_accepted_(
+    const std::shared_ptr<RecordAudioGoalHandle> goal_handle) {}
+
+void AudioBufferNode::record_audio_(const std::shared_ptr<RecordAudioGoalHandle> goal_handle) {}
 } // end of namespace whisper
