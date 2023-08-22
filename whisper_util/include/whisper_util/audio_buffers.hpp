7830402024317020b151097d097fb4c2c16e55ab
mhubii <m.huber_1994@hotmail.de>
2023-08-22 13:08:50 +0100

    added ring buffer

--- whisper_util/include/whisper_util/audio_buffers.hpp
+++ whisper_util/include/whisper_util/audio_buffers.hpp
@@ -10,11 +10,38 @@
 #include "whisper.h"
 
 namespace whisper {
+std::size_t time_to_sample_count(const std::chrono::milliseconds &ms);
+
+class RingBuffer {
+public:
+  RingBuffer(const std::size_t &capacity);
+
+  void insert(const std::vector<std::int16_t> &data);
+  std::vector<float> retrieve();
+  void reset();
+
+  inline const std::size_t &capacity() const { return capacity_; };
+  inline const std::size_t &size() const { return size_; };
+
+protected:
+  inline std::size_t increment_index_(const std::size_t &index) const {
+    return (index + 1) % capacity_;
+  }
+
+  std::size_t capacity_;
+  std::vector<std::int16_t> buffer_;
+  std::mutex mutex_;
+  std::size_t head_;
+  std::size_t tail_;
+  std::size_t size_;
+};
+
 class EpisodicBuffer {
 public:
-  EpisodicBuffer(const std::chrono::milliseconds &max_capacity = std::chrono::seconds(10),
+  EpisodicBuffer(const std::chrono::milliseconds &episode_capacity = std::chrono::seconds(10),
+                 const std::chrono::milliseconds &audio_buffer_capacity = std::chrono::seconds(2),
                  const std::chrono::milliseconds &carry_over = std::chrono::milliseconds(200));
-  void append_new_audio(const std::vector<std::int16_t> &audio);
+  void insert(const std::vector<std::int16_t> &audio);
   void append_audio_from_new();
 
   inline std::vector<float> get_audio() const { return audio_; }
@@ -22,14 +49,13 @@ public:
 protected:
   void clear_audio_();
 
-  std::size_t time_to_sample_count_(const std::chrono::milliseconds &ms);
-
   std::mutex mutex_;
 
-  std::size_t max_capacity_;
+  std::size_t episode_capacity_;
   std::size_t carry_over_;
 
-  std::vector<float> audio_, audio_new_;
+  std::vector<float> audio_;
+  RingBuffer audio_buffer_;
 };
 } // end of namespace whisper
 #endif // WHISPER_UTIL__AUDIO_BUFFERS_HPP_
--- whisper_util/src/audio_buffers.cpp
+++ whisper_util/src/audio_buffers.cpp
@@ -1,35 +1,76 @@
 #include "whisper_util/audio_buffers.hpp"
 
 namespace whisper {
-EpisodicBuffer::EpisodicBuffer(const std::chrono::milliseconds &max_capacity,
+std::size_t time_to_sample_count(const std::chrono::milliseconds &ms) {
+  return ms.count() * WHISPER_SAMPLE_RATE / 1e3;
+}
+
+RingBuffer::RingBuffer(const std::size_t &capacity) : capacity_(capacity), buffer_(capacity) {
+  reset();
+};
+
+void RingBuffer::insert(const std::vector<std::int16_t> &data) {
+  std::lock_guard<std::mutex> lock(mutex_);
+  for (const auto &sample : data) {
+    buffer_[head_] = sample;
+    head_ = increment_index_(head_);
+    ++size_;
+  }
+}
+
+std::vector<float> RingBuffer::retrieve() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  std::vector<float> data(size_);
+  std::for_each(data.begin(), data.end(), [this](float &sample) {
+    sample = static_cast<float>(buffer_[tail_]) /
+             static_cast<float>(std::numeric_limits<std::int16_t>::max());
+    tail_ = increment_index_(tail_);
+  });
+  size_ = 0;
+  return data;
+}
+
+void RingBuffer::reset() {
+  std::lock_guard<std::mutex> lock(mutex_);
+  head_ = 0;
+  tail_ = 0;
+  size_ = 0;
+}
+
+EpisodicBuffer::EpisodicBuffer(const std::chrono::milliseconds &episode_capacity,
+                               const std::chrono::milliseconds &audio_buffer_capacity,
                                const std::chrono::milliseconds &carry_over)
-    : max_capacity_(time_to_sample_count_(max_capacity)),
-      carry_over_(time_to_sample_count_(carry_over)){
+    : episode_capacity_(time_to_sample_count(episode_capacity)),
+      carry_over_(time_to_sample_count(carry_over)),
+      audio_buffer_(time_to_sample_count(audio_buffer_capacity)){
 
       };
 
-void EpisodicBuffer::append_new_audio(const std::vector<std::int16_t> &audio) {
-  // TODO: this must be a ring buffer!
+void EpisodicBuffer::insert(const std::vector<std::int16_t> &audio) {
   std::lock_guard<std::mutex> lock(mutex_);
-  std::transform(audio.begin(), audio.end(), std::back_inserter(audio_new_),
-                 [](const std::int16_t &sample) {
-                   return static_cast<float>(sample) /
-                          static_cast<float>(std::numeric_limits<std::int16_t>::max());
-                 });
+  audio_buffer_.insert(audio);
 }
 
 void EpisodicBuffer::append_audio_from_new() {
   std::lock_guard<std::mutex> lock(mutex_);
-  if (audio_new_.empty()) {
-    return;
+  if (audio_buffer_.size() == 0) {
+    // warn
   }
 
-  if (audio_.size() + audio_new_.size() > max_capacity_) {
+  if (audio_buffer_.size() > audio_buffer_.capacity()) {
+    // warn
+  }
+
+  if (audio_.size() + audio_buffer_.size() > episode_capacity_) {
+    // not clear how this should work. How to keep track of what data has been inferenced on?
+    std::cout << "clearing audio buffer: "
+              << " " << audio_.size() << " + " << audio_buffer_.size() << " < " << episode_capacity_
+              << std::endl;
     clear_audio_();
   }
 
-  audio_.insert(audio_.end(), audio_new_.begin(), audio_new_.end());
-  audio_new_.clear();
+  auto audio_new = audio_buffer_.retrieve();
+  audio_.insert(audio_.end(), audio_new.begin(), audio_new.end());
 }
 
 void EpisodicBuffer::clear_audio_() {
@@ -37,9 +78,5 @@ void EpisodicBuffer::clear_audio_() {
   std::vector<float> carry_over(audio_.end() - carry_over_, audio_.end());
   audio_.clear();
   audio_.insert(audio_.end(), carry_over.begin(), carry_over.end());
-}
-
-std::size_t EpisodicBuffer::time_to_sample_count_(const std::chrono::milliseconds &ms) {
-  return ms.count() * WHISPER_SAMPLE_RATE / 1e3;
 }
 } // end of namespace whisper
