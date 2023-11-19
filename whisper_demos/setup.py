d8d8523acdaf3712c8452311fff5733c62f2b67f
mhubii <m.huber_1994@hotmail.de>
2023-11-19 19:29:09 +0000

    bumped version 1.1.0 -> 1.2.0

--- whisper_demos/setup.py
+++ whisper_demos/setup.py
@@ -4,7 +4,7 @@ package_name = "whisper_demos"
 
 setup(
     name=package_name,
-    version="1.1.0",
+    version="1.2.0",
     packages=find_packages(exclude=["test"]),
     data_files=[
         ("share/ament_index/resource_index/packages", ["resource/" + package_name]),
