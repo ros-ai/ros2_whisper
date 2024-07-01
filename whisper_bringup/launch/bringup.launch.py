7f17429989ecec3cc5ed9741d78d7ca2187f2d9f
mhubii <m.huber_1994@hotmail.de>
2024-07-01 17:31:22 +0100

    fixed parameters

--- whisper_bringup/launch/bringup.launch.py
+++ whisper_bringup/launch/bringup.launch.py
@@ -1,5 +1,6 @@
 import os
 
+import yaml
 from ament_index_python import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import ComposableNodeContainer, Node
@@ -18,10 +19,15 @@ def generate_launch_description() -> LaunchDescription:
         )
     )
 
-    # launch whisper
+    # read configs for composable node: https://answers.ros.org/question/348324/ros2-how-do-you-load-a-yaml-config-file-to-a-composable-node/
     whisper_config = os.path.join(
         get_package_share_directory("whisper_server"), "config", "whisper.yaml"
     )
+    whisper_config = yaml.safe_load(open(whisper_config, "r"))["inference"][
+        "ros__parameters"
+    ]
+
+    # launch whisper inference server in multi-threaded container
     composable_node = ComposableNode(
         package="whisper_server",
         plugin="whisper::InferenceComponent",
