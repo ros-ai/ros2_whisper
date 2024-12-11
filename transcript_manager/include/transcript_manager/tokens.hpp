3983d1bfb1e198475308a9aacdbc465578ff6443
Nathan Corral <nathanbcorral@gmail.com>
2024-12-11 11:34:06 +0100

    Removed un-needed constructors

--- transcript_manager/include/transcript_manager/tokens.hpp
+++ transcript_manager/include/transcript_manager/tokens.hpp
@@ -13,7 +13,6 @@ class SingleToken {
 private:
   std::string data_;
   float prob_;
-  int token_id_;
 
 public:
   std::string get_data() const {
@@ -26,34 +25,6 @@ public:
 
   SingleToken(const std::string& data_, float prob_)
         : data_(data_), prob_(prob_) {};
-
-  // Copy constructor
-  SingleToken(const SingleToken& other)
-      : data_(other.data_), prob_(other.prob_), token_id_(other.token_id_) {};
-
-  // Move constructor
-  SingleToken(SingleToken&& other) noexcept
-      : data_(std::move(other.data_)), prob_(other.prob_), token_id_(other.token_id_) {};
-
-  // Copy assignment operator
-  SingleToken& operator=(const SingleToken& other) {
-    if ( this != &other ) {
-      data_ = other.data_;
-      prob_ = other.prob_;
-      token_id_ = other.token_id_;
-    }
-    return *this;
-  }
-
-  // Move assignment operator
-  SingleToken& operator=(SingleToken&& other) noexcept {
-    if ( this != &other ) {
-      data_ = std::move(other.data_);
-      prob_ = other.prob_;
-      token_id_ = other.token_id_;
-    }
-    return *this;
-  }
 };
 
 } // end of namespace whisper
